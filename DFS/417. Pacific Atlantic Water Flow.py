8d47d98c695fefb8c087e7333fdf06951e8c9bab
Sonawane, Aditi Dilip <asonawan@iu.edu>
2023-11-29 20:13:46 -0500

    Create 417. Pacific Atlantic Water Flow.py

---
+++ DFS/417. Pacific Atlantic Water Flow.py
@@ -0,0 +1,37 @@
+# 417. Pacific Atlantic Water Flow
+
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+
+        rows, cols = len(heights), len(heights[0])
+        pacific = set()
+        atlantic = set()
+        result = []
+
+        def dfs(row, col, prev, visited):
+            if (row, col) in visited or row<0 or col<0 or row>=rows or col>=cols or heights[row][col]<prev:
+                return
+
+            visited.add((row,col))
+
+            dfs(row+1, col, heights[row][col], visited)
+            dfs(row-1, col, heights[row][col], visited)
+            dfs(row, col+1, heights[row][col], visited)
+            dfs(row, col-1, heights[row][col], visited)
+        
+        for row in range(rows):
+            dfs(row,0,heights[row][0],pacific)
+            dfs(row,rows-1,heights[row][rows-1],atlantic)
+
+        for col in range(cols):
+            dfs(0,col,heights[0][col],pacific)
+            dfs(cols-1,col,heights[cols-1][col],atlantic)
+
+        for row in range(rows):
+            for col in range(cols):
+                if (row, col) in atlantic and (row, col) in pacific:
+                    result.append([row, col])
+
+        return result
+
+# Time and Space Complexities - O(rows * cols)
